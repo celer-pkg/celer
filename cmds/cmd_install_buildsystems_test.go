eba071d0160ae28e2fab907c1941794d1491c386
celer-pkg <zhangfei_jiayou@outlook.com>
2025-10-29 04:49:13 +0000

    optimize cmd_install_buildsystems_test

--- cmds/cmd_install_buildsystems_test.go
+++ cmds/cmd_install_buildsystems_test.go
@@ -35,8 +35,6 @@ func TestInstall_Buildsystems_Makefiles(t *testing.T) {
 	check(celer.SetBuildType("Release"))
 	check(celer.SetPlatform(expr.If(runtime.GOOS == "windows", "x86_64-windows-msvc-14.44", "x86_64-linux-ubuntu-22.04-gcc-11.5")))
 	check(celer.SetProject(projectName))
-
-	// Setup build envs.
 	check(celer.Platform().Setup())
 
 	var (
@@ -72,207 +70,6 @@ func TestInstall_Buildsystems_Makefiles(t *testing.T) {
 		BuildCache: true,
 	}
 	check(port.Remove(removeOptions))
-
-	t.Run("install cmake", func(t *testing.T) {
-		var (
-			nameVersion   = "glog@0.6.0"
-			platform      = expr.If(runtime.GOOS == "windows", "x86_64-windows-msvc-14.44", "x86_64-linux-ubuntu-22.04-gcc-11.5")
-			packageFolder = fmt.Sprintf("%s@%s@%s@%s", nameVersion, platform, projectName, celer.BuildType())
-
-			port    configs.Port
-			options configs.InstallOptions
-		)
-
-		check(port.Init(celer, nameVersion))
-		check(port.InstallFromSource(options))
-
-		// Check if package dir exists.
-		packageDir := filepath.Join(dirs.PackagesDir, packageFolder)
-
-		if !fileio.PathExists(packageDir) {
-			t.Fatal("package dir cannot found")
-		}
-
-		// Check if installed.
-		installed, err := port.Installed()
-		check(err)
-		if !installed {
-			t.Fatal("package is not installed")
-		}
-
-		// Clean up.
-		removeOptions := configs.RemoveOptions{
-			Purge:      true,
-			Recurse:    true,
-			BuildCache: true,
-		}
-		check(port.Remove(removeOptions))
-	})
-
-	t.Run("install b2", func(t *testing.T) {
-		var (
-			nameVersion   = "boost@1.87.0"
-			platform      = expr.If(runtime.GOOS == "windows", "x86_64-windows-msvc-14.44", "x86_64-linux-ubuntu-22.04-gcc-11.5")
-			packageFolder = fmt.Sprintf("%s@%s@%s@%s", nameVersion, platform, projectName, celer.BuildType())
-
-			port    configs.Port
-			options configs.InstallOptions
-		)
-
-		check(port.Init(celer, nameVersion))
-		check(port.InstallFromSource(options))
-
-		// Check if package dir exists.
-		packageDir := filepath.Join(dirs.PackagesDir, packageFolder)
-		if !fileio.PathExists(packageDir) {
-			t.Fatal("package dir cannot found")
-		}
-
-		// Check if installed.
-		installed, err := port.Installed()
-		check(err)
-		if !installed {
-			t.Fatal("package is not installed")
-		}
-
-		// Clean up.
-		removeOptions := configs.RemoveOptions{
-			Purge:      true,
-			Recurse:    true,
-			BuildCache: true,
-		}
-		check(port.Remove(removeOptions))
-	})
-
-	t.Run("install gyp", func(t *testing.T) {
-		var (
-			nameVersion   = "nss@3.55"
-			platform      = expr.If(runtime.GOOS == "windows", "x86_64-windows-msvc-14.44", "x86_64-linux-ubuntu-22.04-gcc-11.5")
-			packageFolder = fmt.Sprintf("%s@%s@%s@%s", nameVersion, platform, projectName, celer.BuildType())
-
-			port    configs.Port
-			options configs.InstallOptions
-		)
-
-		check(port.Init(celer, nameVersion))
-		check(port.InstallFromSource(options))
-
-		// Check if package dir exists.
-		packageDir := filepath.Join(dirs.PackagesDir, packageFolder)
-		if !fileio.PathExists(packageDir) {
-			t.Fatal("package cannot found")
-		}
-
-		// Check if installed.
-		installed, err := port.Installed()
-		check(err)
-		if !installed {
-			t.Fatal("package is not installed")
-		}
-
-		// Clean up.
-		removeOptions := configs.RemoveOptions{
-			Purge:      true,
-			Recurse:    true,
-			BuildCache: true,
-		}
-		check(port.Remove(removeOptions))
-	})
-
-	t.Run("install meson", func(t *testing.T) {
-		var (
-			nameVersion   = "pixman@0.44.2"
-			platform      = expr.If(runtime.GOOS == "windows", "x86_64-windows-msvc-14.44", "x86_64-linux-ubuntu-22.04-gcc-11.5")
-			packageFolder = fmt.Sprintf("%s@%s@%s@%s", nameVersion, platform, projectName, celer.BuildType())
-
-			port    configs.Port
-			options configs.InstallOptions
-		)
-
-		check(port.Init(celer, nameVersion))
-		check(port.InstallFromSource(options))
-
-		// Check if package dir exists.
-		packageDir := filepath.Join(dirs.PackagesDir, packageFolder)
-		if !fileio.PathExists(packageDir) {
-			t.Fatal("package dir cannot found")
-		}
-
-		// Check if installed.
-		installed, err := port.Installed()
-		check(err)
-		if !installed {
-			t.Fatal("package is not installed")
-		}
-
-		// Clean up.
-		removeOptions := configs.RemoveOptions{
-			Purge:      true,
-			Recurse:    true,
-			BuildCache: true,
-		}
-		check(port.Remove(removeOptions))
-	})
-
-	t.Run("install prebuilt", func(t *testing.T) {
-		var (
-			nameVersion   = "prebuilt-x264@stable"
-			platform      = expr.If(runtime.GOOS == "windows", "x86_64-windows-msvc-14.44", "x86_64-linux-ubuntu-22.04-gcc-11.5")
-			packageFolder = fmt.Sprintf("%s@%s@%s@%s", nameVersion, platform, projectName, celer.BuildType())
-
-			port    configs.Port
-			options configs.InstallOptions
-		)
-
-		check(port.Init(celer, nameVersion))
-		check(port.InstallFromSource(options))
-
-		// Check if package dir exists.
-		packageDir := filepath.Join(dirs.PackagesDir, packageFolder)
-		if !fileio.PathExists(packageDir) {
-			t.Fatal("package cannot found")
-		}
-
-		// Check if installed.
-		installed, err := port.Installed()
-		check(err)
-		if !installed {
-			t.Fatal("package is not installed")
-		}
-
-		// Clean up.
-		removeOptions := configs.RemoveOptions{
-			Purge:      true,
-			Recurse:    true,
-			BuildCache: true,
-		}
-		check(port.Remove(removeOptions))
-	})
-
-	t.Run("install nobuild", func(t *testing.T) {
-		var (
-			nameVersion = "gnulib@master"
-
-			port    configs.Port
-			options configs.InstallOptions
-		)
-
-		check(port.Init(celer, nameVersion))
-		check(port.InstallFromSource(options))
-
-		if !fileio.PathExists(port.MatchedConfig.PortConfig.RepoDir) {
-			t.Fatal("src dir cannot found")
-		}
-
-		// Clean up.
-		removeOptions := configs.RemoveOptions{
-			Purge:      true,
-			Recurse:    true,
-			BuildCache: true,
-		}
-		check(port.Remove(removeOptions))
-	})
-
 }
 
 func TestInstall_Buildsystems_CMake(t *testing.T) {
@@ -298,8 +95,6 @@ func TestInstall_Buildsystems_CMake(t *testing.T) {
 	check(celer.SetBuildType("Release"))
 	check(celer.SetPlatform(expr.If(runtime.GOOS == "windows", "x86_64-windows-msvc-14.44", "x86_64-linux-ubuntu-22.04-gcc-11.5")))
 	check(celer.SetProject(projectName))
-
-	// Setup build envs.
 	check(celer.Platform().Setup())
 
 	var (
@@ -360,8 +155,6 @@ func TestInstall_Buildsystems_B2(t *testing.T) {
 	check(celer.SetBuildType("Release"))
 	check(celer.SetPlatform(expr.If(runtime.GOOS == "windows", "x86_64-windows-msvc-14.44", "x86_64-linux-ubuntu-22.04-gcc-11.5")))
 	check(celer.SetProject(projectName))
-
-	// Setup build envs.
 	check(celer.Platform().Setup())
 
 	var (
@@ -421,8 +214,6 @@ func TestInstall_Buildsystems_Gyp(t *testing.T) {
 	check(celer.SetBuildType("Release"))
 	check(celer.SetPlatform(expr.If(runtime.GOOS == "windows", "x86_64-windows-msvc-14.44", "x86_64-linux-ubuntu-22.04-gcc-11.5")))
 	check(celer.SetProject(projectName))
-
-	// Setup build envs.
 	check(celer.Platform().Setup())
 
 	var (
@@ -482,8 +273,6 @@ func TestInstall_Buildsystems_Meson(t *testing.T) {
 	check(celer.SetBuildType("Release"))
 	check(celer.SetPlatform(expr.If(runtime.GOOS == "windows", "x86_64-windows-msvc-14.44", "x86_64-linux-ubuntu-22.04-gcc-11.5")))
 	check(celer.SetProject(projectName))
-
-	// Setup build envs.
 	check(celer.Platform().Setup())
 
 	var (
@@ -543,8 +332,6 @@ func TestInstall_Buildsystems_Prebuilt(t *testing.T) {
 	check(celer.SetBuildType("Release"))
 	check(celer.SetPlatform(expr.If(runtime.GOOS == "windows", "x86_64-windows-msvc-14.44", "x86_64-linux-ubuntu-22.04-gcc-11.5")))
 	check(celer.SetProject(projectName))
-
-	// Setup build envs.
 	check(celer.Platform().Setup())
 
 	var (
@@ -604,8 +391,6 @@ func TestInstall_Buildsystems_PNobuild(t *testing.T) {
 	check(celer.SetBuildType("Release"))
 	check(celer.SetPlatform(expr.If(runtime.GOOS == "windows", "x86_64-windows-msvc-14.44", "x86_64-linux-ubuntu-22.04-gcc-11.5")))
 	check(celer.SetProject(projectName))
-
-	// Setup build envs.
 	check(celer.Platform().Setup())
 
 	var (
