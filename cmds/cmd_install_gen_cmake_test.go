114b32cedb46706e482fe9e3b07f26ed5993e8c2
phil.zhang <741152074@qq.com>
2025-11-16 22:04:11 +0800

    fix unit test for windows

--- cmds/cmd_install_gen_cmake_test.go
+++ cmds/cmd_install_gen_cmake_test.go
@@ -117,6 +117,10 @@ func TestInstall_Generate_CMake_Prebuilt_Interface_Libraries(t *testing.T) {
 	check(celer.SetProject(project))
 	check(celer.Setup())
 
+	if err := buildtools.CheckTools(celer, "git", "cmake"); err != nil {
+		t.Fatal(err)
+	}
+
 	var port configs.Port
 	var options configs.InstallOptions
 	check(port.Init(celer, nameVersion))
@@ -131,9 +135,6 @@ func TestInstall_Generate_CMake_Prebuilt_Interface_Libraries(t *testing.T) {
 	})
 
 	// Build test project.
-	if err := buildtools.CheckTools(celer, "cmake"); err != nil {
-		t.Fatal(err)
-	}
 	executer := cmd.NewExecutor("configure test project", "cmake",
 		"-D", fmt.Sprintf("CMAKE_TOOLCHAIN_FILE=%s/toolchain_file.cmake", dirs.WorkspaceDir),
 		"-S", filepath.Join(dirs.WorkspaceDir, "testdata/gen_cmake_configs_prebuilt/interface_libraries"),
@@ -188,6 +189,10 @@ func TestInstall_Generate_CMake_Prebuilt_Components(t *testing.T) {
 	check(celer.SetProject(project))
 	check(celer.Setup())
 
+	if err := buildtools.CheckTools(celer, "git", "cmake"); err != nil {
+		t.Fatal(err)
+	}
+
 	var port configs.Port
 	var options configs.InstallOptions
 	check(port.Init(celer, nameVersion))
@@ -202,9 +207,6 @@ func TestInstall_Generate_CMake_Prebuilt_Components(t *testing.T) {
 	})
 
 	// Build test project.
-	if err := buildtools.CheckTools(celer, "cmake"); err != nil {
-		t.Fatal(err)
-	}
 	executer := cmd.NewExecutor("configure test project", "cmake",
 		"-D", fmt.Sprintf("CMAKE_TOOLCHAIN_FILE=%s/toolchain_file.cmake", dirs.WorkspaceDir),
 		"-S", filepath.Join(dirs.WorkspaceDir, "testdata/gen_cmake_configs_prebuilt/components"),
@@ -259,6 +261,10 @@ func TestInstall_Generate_CMake_Source_Single_Target(t *testing.T) {
 	check(celer.SetProject(project))
 	check(celer.Setup())
 
+	if err := buildtools.CheckTools(celer, "git", "cmake"); err != nil {
+		t.Fatal(err)
+	}
+
 	var port configs.Port
 	var options configs.InstallOptions
 	check(port.Init(celer, nameVersion))
@@ -272,9 +278,6 @@ func TestInstall_Generate_CMake_Source_Single_Target(t *testing.T) {
 	})
 
 	// Build test project.
-	if err := buildtools.CheckTools(celer, "cmake"); err != nil {
-		t.Fatal(err)
-	}
 	executer := cmd.NewExecutor("configure test project", "cmake",
 		"-D", fmt.Sprintf("CMAKE_TOOLCHAIN_FILE=%s/toolchain_file.cmake", dirs.WorkspaceDir),
 		"-S", filepath.Join(dirs.WorkspaceDir, "testdata/gen_cmake_configs_source/single_target"),
@@ -329,6 +332,10 @@ func TestInstall_Generate_CMake_Source_Components(t *testing.T) {
 	check(celer.SetProject(project))
 	check(celer.Setup())
 
+	if err := buildtools.CheckTools(celer, "git", "cmake"); err != nil {
+		t.Fatal(err)
+	}
+
 	var port configs.Port
 	var options configs.InstallOptions
 	check(port.Init(celer, nameVersion))
@@ -342,9 +349,6 @@ func TestInstall_Generate_CMake_Source_Components(t *testing.T) {
 	})
 
 	// Build test project.
-	if err := buildtools.CheckTools(celer, "cmake"); err != nil {
-		t.Fatal(err)
-	}
 	executer := cmd.NewExecutor("configure test project", "cmake",
 		"-D", fmt.Sprintf("CMAKE_TOOLCHAIN_FILE=%s/toolchain_file.cmake", dirs.WorkspaceDir),
 		"-S", filepath.Join(dirs.WorkspaceDir, "testdata/gen_cmake_configs_source/components"),
