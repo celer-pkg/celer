4edaf4fa8dfe8bb53f5713baa75a0beb24378901
phil-zhang <741152074@qq.com>
2025-12-25 23:37:59 +0800

    continue improve

--- cmds/cmd_install_gen_cmake_config_test.go
+++ cmds/cmd_install_gen_cmake_config_test.go
@@ -13,7 +13,7 @@ import (
 	"testing"
 )
 
-func TestInstall_Generate_CMake_Prebuilt_Single_Target(t *testing.T) {
+func TestInstall_Generate_CMake_Config_Single(t *testing.T) {
 	// Cleanup.
 	dirs.RemoveAllForTest()
 
@@ -60,7 +60,7 @@ func TestInstall_Generate_CMake_Prebuilt_Single_Target(t *testing.T) {
 	}
 	executer := cmd.NewExecutor("configure test project", "cmake",
 		"-D", fmt.Sprintf("CMAKE_TOOLCHAIN_FILE=%s/toolchain_file.cmake", dirs.WorkspaceDir),
-		"-S", filepath.Join(dirs.WorkspaceDir, "testdata/gen_cmake_configs_prebuilt/single_target"),
+		"-S", filepath.Join(dirs.WorkspaceDir, "testdata/gen_cmake_config/single"),
 		"-B", buildDir,
 	)
 	executer.SetWorkDir(buildDir)
@@ -78,7 +78,7 @@ func TestInstall_Generate_CMake_Prebuilt_Single_Target(t *testing.T) {
 	}))
 }
 
-func TestInstall_Generate_CMake_Prebuilt_Interface_Libraries(t *testing.T) {
+func TestInstall_Generate_CMake_Config_Interface(t *testing.T) {
 	// Cleanup.
 	dirs.RemoveAllForTest()
 
@@ -122,7 +122,7 @@ func TestInstall_Generate_CMake_Prebuilt_Interface_Libraries(t *testing.T) {
 	// Build test project.
 	executer := cmd.NewExecutor("configure test project", "cmake",
 		"-D", fmt.Sprintf("CMAKE_TOOLCHAIN_FILE=%s/toolchain_file.cmake", dirs.WorkspaceDir),
-		"-S", filepath.Join(dirs.WorkspaceDir, "testdata/gen_cmake_configs_prebuilt/interface_libraries"),
+		"-S", filepath.Join(dirs.WorkspaceDir, "testdata/gen_cmake_config/interface"),
 		"-B", buildDir,
 	)
 	executer.SetWorkDir(buildDir)
@@ -140,7 +140,7 @@ func TestInstall_Generate_CMake_Prebuilt_Interface_Libraries(t *testing.T) {
 	}))
 }
 
-func TestInstall_Generate_CMake_Prebuilt_Components(t *testing.T) {
+func TestInstall_Generate_CMake_Config_Components(t *testing.T) {
 	// Cleanup.
 	dirs.RemoveAllForTest()
 
@@ -184,7 +184,7 @@ func TestInstall_Generate_CMake_Prebuilt_Components(t *testing.T) {
 	// Build test project.
 	executer := cmd.NewExecutor("configure test project", "cmake",
 		"-D", fmt.Sprintf("CMAKE_TOOLCHAIN_FILE=%s/toolchain_file.cmake", dirs.WorkspaceDir),
-		"-S", filepath.Join(dirs.WorkspaceDir, "testdata/gen_cmake_configs_prebuilt/components"),
+		"-S", filepath.Join(dirs.WorkspaceDir, "testdata/gen_cmake_config/components"),
 		"-B", buildDir,
 	)
 	executer.SetWorkDir(buildDir)
@@ -202,131 +202,7 @@ func TestInstall_Generate_CMake_Prebuilt_Components(t *testing.T) {
 	}))
 }
 
-func TestInstall_Generate_CMake_Source_Single_Target(t *testing.T) {
-	// Cleanup.
-	dirs.RemoveAllForTest()
-
-	// Check error.
-	var check = func(err error) {
-		t.Helper()
-		if err != nil {
-			t.Fatal(err)
-		}
-	}
-
-	// Init celer.
-	celer := configs.NewCeler()
-	check(celer.Init())
-
-	var (
-		nameVersion     = "x264@stable"
-		windowsPlatform = expr.If(os.Getenv("GITHUB_ACTIONS") == "true", "x86_64-windows-msvc-enterprise-14.44", "x86_64-windows-msvc-community-14.44")
-		platform        = expr.If(runtime.GOOS == "windows", windowsPlatform, "x86_64-linux-ubuntu-22.04-gcc-11.5.0")
-		project         = "project_test_install"
-	)
-
-	check(celer.CloneConf("https://github.com/celer-pkg/test-conf.git", "optimize_generating_cmake_config", true))
-	check(celer.SetBuildType("Release"))
-	check(celer.SetPlatform(platform))
-	check(celer.SetProject(project))
-	check(celer.Setup())
-	check(buildtools.CheckTools(celer, "git", "cmake"))
-
-	var port configs.Port
-	var options configs.InstallOptions
-	check(port.Init(celer, nameVersion))
-	_, err := port.Install(options)
-	check(err)
-
-	// Clear build dir.
-	buildDir := filepath.Join(os.TempDir(), "build_cmake_test")
-	check(os.RemoveAll(buildDir))
-	check(os.MkdirAll(buildDir, os.ModePerm))
-
-	// Build test project.
-	executer := cmd.NewExecutor("configure test project", "cmake",
-		"-D", fmt.Sprintf("CMAKE_TOOLCHAIN_FILE=%s/toolchain_file.cmake", dirs.WorkspaceDir),
-		"-S", filepath.Join(dirs.WorkspaceDir, "testdata/gen_cmake_configs_source/single_target"),
-		"-B", buildDir,
-	)
-	executer.SetWorkDir(buildDir)
-	check(executer.Execute())
-
-	executer = cmd.NewExecutor("build test project", "cmake", "--build", buildDir)
-	executer.SetWorkDir(buildDir)
-	check(executer.Execute())
-
-	// Clear up.
-	check(port.Remove(configs.RemoveOptions{
-		Purge:      true,
-		BuildCache: true,
-		Recursive:  true,
-	}))
-}
-
-func TestInstall_Generate_CMake_Source_Components(t *testing.T) {
-	// Cleanup.
-	dirs.RemoveAllForTest()
-
-	// Check error.
-	var check = func(err error) {
-		t.Helper()
-		if err != nil {
-			t.Fatal(err)
-		}
-	}
-
-	// Init celer.
-	celer := configs.NewCeler()
-	check(celer.Init())
-
-	var (
-		nameVersion     = "ffmpeg@5.1.6"
-		windowsPlatform = expr.If(os.Getenv("GITHUB_ACTIONS") == "true", "x86_64-windows-msvc-enterprise-14.44", "x86_64-windows-msvc-community-14.44")
-		platform        = expr.If(runtime.GOOS == "windows", windowsPlatform, "x86_64-linux-ubuntu-22.04-gcc-11.5.0")
-		project         = "project_test_install"
-	)
-
-	check(celer.CloneConf("https://github.com/celer-pkg/test-conf.git", "optimize_generating_cmake_config", true))
-	check(celer.SetBuildType("Release"))
-	check(celer.SetPlatform(platform))
-	check(celer.SetProject(project))
-	check(celer.Setup())
-	check(buildtools.CheckTools(celer, "git", "cmake"))
-
-	var port configs.Port
-	var options configs.InstallOptions
-	check(port.Init(celer, nameVersion))
-	_, err := port.Install(options)
-	check(err)
-
-	// Clear build dir.
-	buildDir := filepath.Join(os.TempDir(), "build_cmake_test")
-	check(os.RemoveAll(buildDir))
-	check(os.MkdirAll(buildDir, os.ModePerm))
-
-	// Build test project.
-	executer := cmd.NewExecutor("configure test project", "cmake",
-		"-D", fmt.Sprintf("CMAKE_TOOLCHAIN_FILE=%s/toolchain_file.cmake", dirs.WorkspaceDir),
-		"-S", filepath.Join(dirs.WorkspaceDir, "testdata/gen_cmake_configs_source/components"),
-		"-B", buildDir,
-	)
-	executer.SetWorkDir(buildDir)
-	check(executer.Execute())
-
-	executer = cmd.NewExecutor("build test project", "cmake", "--build", buildDir)
-	executer.SetWorkDir(buildDir)
-	check(executer.Execute())
-
-	// Clear up.
-	check(port.Remove(configs.RemoveOptions{
-		Purge:      true,
-		BuildCache: true,
-		Recursive:  true,
-	}))
-}
-
-func TestInstall_Generate_CMake_Prebuilt_Interface_Head_Only(t *testing.T) {
+func TestInstall_Generate_CMake_Interface_Head_Only(t *testing.T) {
 	// Cleanup.
 	dirs.RemoveAllForTest()
 
@@ -370,7 +246,7 @@ func TestInstall_Generate_CMake_Prebuilt_Interface_Head_Only(t *testing.T) {
 	// Build test project.
 	executer := cmd.NewExecutor("configure test project", "cmake",
 		"-D", fmt.Sprintf("CMAKE_TOOLCHAIN_FILE=%s/toolchain_file.cmake", dirs.WorkspaceDir),
-		"-S", filepath.Join(dirs.WorkspaceDir, "testdata/gen_cmake_configs_prebuilt/interface_head_only"),
+		"-S", filepath.Join(dirs.WorkspaceDir, "testdata/gen_cmake_config/interface_head_only"),
 		"-B", buildDir,
 	)
 	executer.SetWorkDir(buildDir)
